8f0eac84dcf6b12655a77294a1597f5339776c49
Danilo Melo <danilomarquesdemelo@gmail.com>
2024-06-27 21:24:48 -0300

    feat: create docker compose file for database service and .sql scripts

---
+++ scripts/database/todoTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE todos (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  completed BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+ISERT INTO todos (user_id, title, description) VALUES ('', '', '');
