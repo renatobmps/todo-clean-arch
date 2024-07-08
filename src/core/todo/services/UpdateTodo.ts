a1b635f7e212a91b3e629128f4c5e16f5d7e8d20
Danilo Melo <danilomarquesdemelo@gmail.com>
2024-07-08 16:53:23 -0300

    create: UpdateTodo.ts use case

---
+++ src/core/todo/services/UpdateTodo.ts
@@ -0,0 +1,19 @@
+import IUseCase from "@/core/shared/IUseCase"
+import ITodo from "../models/ITodo"
+import ITodoRepository from "./ITodoRepository"
+
+export default class UpdateTodo implements IUseCase<ITodo, void> {
+  constructor(
+    private readonly todoRepository: ITodoRepository
+  ) { }
+
+  async execute(todo: ITodo): Promise<void> {
+    try {
+      await this.todoRepository.edit(todo)
+
+    } catch (error: any) {
+      console.error(`Error during update to-do id: ${todo.id} from user id: ${todo.userId}: `, error)
+      throw new Error(error.message)
+    }
+  }
+}
