6deebc9745512910c30bd906be04961d091736d3
Danilo Melo <danilomarquesdemelo@gmail.com>
2024-07-13 22:09:30 -0300

    refactor: LoginUserController.test.ts

--- test/unit/src/external/api/controllers/LoginUserController.test.ts
+++ test/unit/src/external/api/controllers/LoginUserController.test.ts
@@ -1,4 +1,3 @@
-import errors from "@/core/shared/errors"
 import IUser from "@/core/user/models/IUser"
 import IPasswordCryptography from "@/core/user/services/IPasswordCryptography"
 import IUserRepository from "@/core/user/services/IUserRepository"
@@ -6,13 +5,12 @@ import LoginUser, { InputUserData } from "@/core/user/services/LoginUser"
 import LoginUserController from "@/external/api/controllers/LoginUserController"
 import express, { Express } from "express"
 import request from "supertest"
-import JwtService from "@/external/auth/JwtService";
 
 jest.mock('@/external/auth/JwtService');
 
 describe("Test LoginUserController.ts", () => {
   let app: Express
-  let loginUser: LoginUser
+  let loginUserUseCaseMock: LoginUser
   let mockUserRepository: jest.Mocked<IUserRepository>
   let mockCryptographyService: jest.Mocked<IPasswordCryptography>
 
@@ -31,19 +29,50 @@ describe("Test LoginUserController.ts", () => {
       compare: jest.fn()
     }
 
-    loginUser = new LoginUser(mockUserRepository, mockCryptographyService)
+    loginUserUseCaseMock = new LoginUser(mockUserRepository, mockCryptographyService)
 
-    new LoginUserController(app, loginUser)
+    new LoginUserController(app, loginUserUseCaseMock)
 
-    jest.spyOn(loginUser, "execute")
+    jest.spyOn(loginUserUseCaseMock, "execute")
+  })
 
+  it("Should return 400 when the credentials are incorrect", async () => {
+    (loginUserUseCaseMock.execute as jest.Mock).mockRejectedValue(new Error("Invalid email or password."))
 
+    const user: IUser = { name: "John Doe", email: "john@email.com", password: "wrong_password" }
+
+    mockUserRepository.readByEmail.mockResolvedValue(user)
+    mockCryptographyService.compare.mockResolvedValue(false)
+
+    const response = await request(app)
+      .post("/api/users/login")
+      .send(user)
+
+    expect(response.status).toBe(400)
+    expect(response.text).toBe("Invalid email or password.")
+    expect(loginUserUseCaseMock.execute).toHaveBeenCalledWith({ email: "john@email.com", password: "wrong_password" })
+  })
+
+  it("Should return 400 when the user dont exists", async () => {
+    (loginUserUseCaseMock.execute as jest.Mock).mockRejectedValue(new Error("User does not exist."))
+
+    const user: InputUserData = { email: "non_existent_user@email.com", password: "password123" }
+
+    mockUserRepository.readByEmail.mockResolvedValue(null)
+
+    const response = await request(app)
+      .post("/api/users/login")
+      .send(user)
+
+    expect(response.status).toBe(400)
+    expect(response.text).toBe("User does not exist.")
+    expect(loginUserUseCaseMock.execute).toHaveBeenCalledWith(user)
   })
 
   it("Should log in an existing user", async () => {
-    (loginUser.execute as jest.Mock).mockResolvedValue({ id: "1", name: "John Doe", email: "john@email.com", password: undefined })
+    (loginUserUseCaseMock.execute as jest.Mock).mockResolvedValue({ name: "John Doe", email: "john@email.com", password: undefined })
 
-    const user: IUser = { id: "1", name: "John Doe", email: "john@email.com", password: "encryptedP4ssW0rd@123" }
+    const user: IUser = { name: "John Doe", email: "john@email.com", password: "encryptedP4ssW0rd@123" }
 
     mockUserRepository.readByEmail.mockResolvedValue(user)
     mockCryptographyService.compare.mockResolvedValue(true)
@@ -55,39 +84,12 @@ describe("Test LoginUserController.ts", () => {
     expect(response.status).toBe(200)
 
     expect(response.body).toEqual({
-      user: { id: "1", name: "John Doe", email: "john@email.com" }
+      user: { name: "John Doe", email: "john@email.com" }
     })
 
-    expect(loginUser.execute).toHaveBeenCalledWith({
+    expect(loginUserUseCaseMock.execute).toHaveBeenCalledWith({
       email: "john@email.com",
       password: "P4ssW0rd@123"
     })
-  })
-
-  it("Should return 400 when the credentials are incorrect", async () => {
-    const user: IUser = { id: "1", name: "John Doe", email: "john@email.com", password: "wrongPassword" }
-
-    mockUserRepository.readByEmail.mockResolvedValue(user)
-    mockCryptographyService.compare.mockResolvedValue(false)
-
-    const response = await request(app)
-      .post("/api/users/login")
-      .send(user)
-
-    expect(response.status).toBe(400)
-    expect(response.text).toBe(errors.INVALID_CREDENTIALS)
-  })
-
-  it("Should return 400 when the user dont exists", async () => {
-    const user: InputUserData = { email: "nonExistentUser@email.com", password: "password123" }
-
-    mockUserRepository.readByEmail.mockResolvedValue(null)
-
-    const response = await request(app)
-      .post("/api/users/login")
-      .send(user)
-
-    expect(response.status).toBe(400)
-    expect(response.text).toBe(errors.USER_DONT_EXISTS)
   })
 })
